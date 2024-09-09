a28dda6386eb3c59193e336132ca5657e7e24be3
Cassy <cassandra.schildknecht@gmail.com>
2024-09-09 17:15:26 +0200

    Initial commit

---
+++ src/main/java/net/cassy/glowingstuffmod/GlowingstuffmodClient.java
@@ -0,0 +1,10 @@
+package net.cassy.glowingstuffmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GlowingstuffmodClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/cassy/glowingstuffmod/GlowingstuffmodDataGenerator.java
@@ -0,0 +1,11 @@
+package net.cassy.glowingstuffmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class GlowingstuffmodDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
